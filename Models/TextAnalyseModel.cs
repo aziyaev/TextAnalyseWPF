ddb61b1f13851a55ffccfa422528f76ab68bfa6b
aziyaev99@gmail.com <aziyaev99@gmail.com>
2024-05-02 00:32:44 +0300

    Добавьте файлы проекта.

---
+++ Models/TextAnalyseModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextAnalyseWPF.Models
+{
+    public class TextAnalyseModel
+    {
+        public double CompareTextsUsingLevenshtein(string textA, string textB, bool ignoreCase)
+        {
+            if (string.IsNullOrWhiteSpace(textA) && string.IsNullOrWhiteSpace(textB))
+            {
+                return 1;
+            }
+            if (string.IsNullOrWhiteSpace(textA) || string.IsNullOrWhiteSpace(textB))
+            {
+                return 0;
+            }
+
+            if (ignoreCase)
+            {
+                textA = textA.ToLower();
+                textB = textB.ToLower();
+            }
+
+            textA = RemovePunctuation(textA);
+            textB = RemovePunctuation(textB);
+
+            return CalculateSimilarityOnLevenshtein(textA, textB);
+        }
+
+        public double CompareTextsUsingNGrams(string textA, string textB, bool ignoreCase)
+        {
+            if (string.IsNullOrWhiteSpace(textA) && string.IsNullOrWhiteSpace(textB))
+            {
+                return 1;
+            }
+            if (string.IsNullOrWhiteSpace(textA) || string.IsNullOrWhiteSpace(textB))
+            {
+                return 0;
+            }
+
+            if (ignoreCase)
+            {
+                textA = textA.ToLower();
+                textB = textB.ToLower();
+            }
+
+            textA = RemovePunctuation(textA);
+            textB = RemovePunctuation(textB);
+
+            return CalculateSimilarityOnNgrams(textA, textB);
+        }
+
+        private double CalculateSimilarityOnLevenshtein(string textA, string textB)
+        {
+            int n = textA.Length;
+            int m = textB.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            if (n == 0)
+                return m;
+            if (m == 0)
+                return n;
+
+            for(int i = 0; i <= n; d[i, 0] = i++) { }
+            for(int j = 0; j <= m; d[0, j] = j++) { }
+
+            for(int i = 1; i <= n; i++)
+            {
+                for(int j = 1; j<= m; j++)
+                {
+                    int cost = (textB[j - 1] == textA[i - 1]) ? 0 : 1;
+
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return 1.0 - ((double)d[n,m] / Math.Max(textA.Length, textB.Length));
+        }
+
+        private double CalculateSimilarityOnNgrams(string textA, string textB)
+        {
+            const int ngrams = 3;
+            var ngramsA = GetNgrams(textA, ngrams);
+            var ngramsB = GetNgrams(textB, ngrams);
+
+            var intersection = ngramsA.Intersect(ngramsB).Count();
+            var union = ngramsA.Union(ngramsB).Count();
+
+            return (double)intersection / union;
+        }
+
+        private IEnumerable<string> GetNgrams(string text, int n)
+        {
+            for(int i = 0; i < text.Length - n + 1; i++)
+            {
+                yield return text.Substring(i, n);
+            }
+        }
+
+        private string RemovePunctuation(string text)
+        {
+            text = text.Trim();
+            return Regex.Replace(text, @"[^\w\s]", "");
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TextAnalyseWPF.Command;
+using TextAnalyseWPF.Models;
+
+namespace TextAnalyseWPF.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public enum AlgorithmType
+        {
+            [Description("Расстояние Левенштейна")]
+            Levenshtein,
+            [Description("N-граммы")]
+            NGrams
+        }
+
+        private string _textA;
+        private string _textB;
+        private double _result;
+        private bool _ignoreCase;
+        private AlgorithmType _selectedAlgorithm = AlgorithmType.Levenshtein;
+        private RelayCommand _compareCommand;
+
+        public string TextA
+        {
+            get { return _textA; }
+            set
+            {
+                _textA = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TextB
+        {
+            get { return _textB; }
+            set
+            {
+                _textB = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ResultText
+        {
+            get
+            {
+                if(string.IsNullOrWhiteSpace(TextA) && string.IsNullOrWhiteSpace(TextB))
+                {
+                    return "Вставьте текст";
+                }
+                else if (string.IsNullOrWhiteSpace(TextA))
+                {
+                    return "Вставьте Текст 1";
+                }
+                else if (string.IsNullOrWhiteSpace(TextB))
+                {
+                    return "Вставьте Текст 2";
+                }
+                else
+                {
+                    int similarityPercent = (int)(Result * 100);
+                    int differentPercent = 100 - similarityPercent;
+                    return $"Процент схожести текстов {similarityPercent}%. Разница составляет {differentPercent}%";
+                }
+            }
+        }
+
+        public bool IgnoreCase
+        {
+            get { return _ignoreCase; }
+            set
+            {
+                _ignoreCase = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AlgorithmType SelectedAlgorithm
+        {
+            get
+            {
+                return _selectedAlgorithm;
+            }
+            set
+            {
+                _selectedAlgorithm = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            
+        }
+
+        public ICommand CompareCommand
+        {
+            get
+            {
+                if (_compareCommand == null)
+                {
+                    _compareCommand = new RelayCommand(Compare);
+                }
+                return _compareCommand;
+            }
+        }
+
+        private void Compare()
+        {
+            var comparer = new TextAnalyseModel();
+            if(SelectedAlgorithm == AlgorithmType.Levenshtein)
+            {
+                Result = comparer.CompareTextsUsingLevenshtein(TextA, TextB, IgnoreCase);
+            }
+            else
+            {
+                Result = comparer.CompareTextsUsingNGrams(TextA, TextB, IgnoreCase);
+            }
+            OnPropertyChanged(nameof(ResultText));
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
